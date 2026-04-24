68e12b919551598c138f463c2cc1a9af86dcabbc
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2026-04-24 18:12:27 +0600

    update email

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -33,8 +33,8 @@ export async function POST(req: Request) {
 
     const mailOptions = {
       from: '"Rivaan Tech" <noreplay@rivaantech.ae>',
-    //   to: "info@rivaantech.ae",
-      to: "mahitasnimul2@gmail.com",
+      to: "info@rivaantech.ae",
+      
       subject: `New Contact Form Submission from ${name}: ${subject}`,
       html,
     };
